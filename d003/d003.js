422b658793ffa263fc5eb2caa9bac45f86a4ea40
sabrinn07 <143205292+sabrinn07@users.noreply.github.com>
2023-12-21 19:31:00 -0300

    Delete d003/d003.js

--- d003/d003.js
+++
@@ -1,5 +0,0 @@
-function AntSuc(){
-    var num = Number(window.prompt('Digite um número para aparecer o sucessor e antecessor desse número'))
-    var valorFinal = window.document.getElementById('resultado');
-    valorFinal.innerHTML = '<p>O sucessor desse número é '+ (num + 1) + ' e o seu antecessor é '+(num - 1);
-}
